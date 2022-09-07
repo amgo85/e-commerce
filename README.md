80597d6dd1dfc2dde2ef83e4098979fcb792c272
Ahmad <amgo85@gmail.com>
2022-09-07 08:38:46 +0530

    Vid 7 & Vid 8 shopping cart & update cart Qty

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 
 ## from Ahmed Sep 06 2022
 I watched fully Video 6 about creating Product Detail Page
-I had the same issue ofy yours that is slider not showing as instructor
+I had the same issue of yours that is slider not showing as instructor
 Changes are pushed
+
+## from Ahmed Sep 07 2022
+Video 7 & Video 8 have been fully completed
+No issues at all.
+Changes were pushed
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Livewire\Admin\AdminDashboardComponent;
-use App\Http\Livewire\CartComponent;
-use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\DetailsComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',HomeComponent::class);
 Route::get('/shop',ShopComponent::class);
-Route::get('/cart',CartComponent::class);
+Route::get('/cart',CartComponent::class)->name('product.cart');
 Route::get('/checkout',CheckoutComponent::class);
 Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
 
