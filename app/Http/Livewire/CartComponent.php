61e384099c4f671bc60fc5a405074e26301c5c85
Ahmad <amgo85@gmail.com>
2022-11-04 22:36:32 +0400

    Video 37 completed

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -2,62 +2,121 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Coupon;
 use Livewire\Component;
 use Cart;
 
 class CartComponent extends Component
 {
+    // Creating new properties 
+    public $haveCouponCode;
+    public $couponCode;
+    public $discount;
+    public $subtotalAfterDiscount;
+    public $taxAfterDiscount;
+    public $totalAfterDiscount;
+
     public function increaseQuantity($rowId){
         $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
         Cart::instance('cart')->update($rowId, $qty);
-        $this->emitTo('cart-count-component', 'refreshComponent');
+        $this->emitTo('cart-count-component','refreshComponent');
     }
 
     public function decreaseQuantity($rowId){
         $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty - 1;
         Cart::instance('cart')->update($rowId, $qty);
-        $this->emitTo('cart-count-component', 'refreshComponent');
+        $this->emitTo('wishlist-count-component','refreshComponent');
     }
 
     public function destroy($rowId){
         Cart::instance('cart')->remove($rowId);
-        $this->emitTo('cart-count-component', 'refreshComponent');
+        $this->emitTo('wishlist-count-component','refreshComponent');
         session()->flash('success_message', ' Item has been removed');
     }
 
     public function destroyAll(){
         Cart::instance('cart')->destroy();
-        $this->emitTo('cart-count-component', 'refreshComponent');
+        $this->emitTo('wishlist-count-component','refreshComponent');
     }
-
+    // New function Eps 35
     public function switchToSaveForLater($rowId)
     {
         $item = Cart::instance('cart')->get($rowId);
         Cart::instance('cart')->remove($rowId);
-        Cart::instance('saveForLater')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
-        $this->emitTo('cart-count-component', 'refreshComponent');
-        session()->flash('success_message', ' Item has been saved for later');
+        Cart::instance('saveForLater')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component','refreshComponent');
+        session()->flash('success_message','Item has been saved for later');
     }
-
     public function moveToCart($rowId)
     {
         $item = Cart::instance('saveForLater')->get($rowId);
         Cart::instance('saveForLater')->remove($rowId);
-        Cart::instance('cart')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
-        $this->emitTo('cart-count-component', 'refreshComponent');
-        session()->flash('s_success_message', ' Item has been moved to cart');
+        Cart::instance('cart')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component','refreshComponent');
+        session()->flash('s_success_message','Item has been moved to cart');
     }
-
     public function deleteFromSaveForLater($rowId)
     {
         Cart::instance('saveForLater')->remove($rowId);
-        session()->flash('s_success_message', ' Item has been removed from save for later');
+        session()->flash('s_success_message','Item has been removed from save for later');
+    }
+     
+    public function applyCouponCode()
+    {
+        $coupon = Coupon::where('code',$this->couponCode)->where('cart_value','<=',Cart::instance('cart')->subtotal())->first();
+        if(!$coupon)
+        {
+            session()->flash('coupon_message','Coupon code is invalid!');
+            return;
+        }
+
+        session()->put('coupon',[
+            'code' => $coupon->code, 
+            'type' => $coupon->type,
+            'value' => $coupon->value, 
+            'cart_value' => $coupon->cart_value
+
+        ]);
     }
 
+    public function calculateDiscounts()
+    {
+        if(session()->has('coupon'))
+        {
+            if(session()->get('coupon')['type'] == 'fixed')
+            {
+                $this->discount = session()->get('coupon')['value'];
+            }
+            else
+            {
+                $this->discount = (Cart::instance('cart')->subtotal() * session()->get('coupon')['value'])/100;
+            }
+            $this->subtotalAfterDiscount = Cart::instance('cart')->subtotal() - $this->discount;
+            $this->taxAfterDiscount = ($this->subtotalAfterDiscount * config('cart.tax'))/100;
+            $this->totalAfterDiscount = $this->subtotalAfterDiscount +$this->taxAfterDiscount;
+
+        }
+    } 
+
+    public function removeCoupon()
+    {
+        session()->forget('coupon');
+    }
     public function render()
     {
+        if(session()->has('coupon'))
+        {
+            if(Cart::instance('cart')->subtotal() < session()->get('coupon')['cart_value'])
+            {
+                session()->forget('coupon');
+            }
+            else 
+            {
+                $this->calculateDiscounts();
+            }
+        }
         return view('livewire.cart-component')->layout('layouts.base');
     }
 }
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -31,13 +31,14 @@
                                         <a class="link-to-product" href="{{ route('product.details', ['slug'=>$item->model->slug]) }}">{{ $item->model->name }}</a>
                                     </div>
                                     <div class="price-field produtc-price"><p class="price">${{ $item->model->regular_price }}</p></div>
+                                   
                                     <div class="quantity">
                                         <div class="quantity-input">
                                             <input type="text" name="product-quatity" value="{{ $item->qty }}" data-max="120" pattern="[0-9]*" >									
                                             <a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{$item->rowId}}')"></a>
                                             <a class="btn btn-reduce" href="#" wire:click.prevent="decreaseQuantity('{{$item->rowId}}')"></a>
                                         </div>
-                                        <p class="text-center"><a href="#" wire:click.prevent="switchToSaveForLater('{{$item->rowId}}')">Save for Later</a></p>
+                                        <p class="text-center"><a href="#" wire:click.prevent="switchToSaveForLater('{{$item->rowId}}')">Save For Later</a></p>
                                     </div>
                                     <div class="price-field sub-total"><p class="price">${{ $item->subtotal }}</p></div>
                                     <div class="delete">
@@ -58,14 +59,39 @@
                     <div class="order-summary">
                         <h4 class="title-box">Order Summary</h4>
                         <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{Cart::instance('cart')->subtotal()}}</b></p>
-                        <p class="summary-info"><span class="title">Tax</span><b class="index">${{Cart::instance('cart')->tax()}}</b></p>
-                        <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
-                        <p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::instance('cart')->total()}}</b></p>
+                        @if(Session::has('coupon'))
+                            <p class="summary-info"><span class="title">Discount ({{Session::get('coupon')['code']}}) <a href="#" wire:click.prevent="removeCoupon"><i class="fa fa-times text-danger"></i></a></span><b class="index"> -${{number_format($discount,2)}}</b></p>  
+                            <p class="summary-info"><span class="title">Subtotal with Discount</span><b class="index">${{number_format($subtotalAfterDiscount,2)}}</b></p>
+                            <p class="summary-info"><span class="title">Tax ({{config('cart.tax')}}%)</span><b class="index">${{number_format($taxAfterDiscount,2)}}</b></p>
+                            <p class="summary-info total-info "><span class="title">Total</span><b class="index">${{number_format($totalAfterDiscount,2)}}</b></p>
+                            @else
+                            <p class="summary-info"><span class="title">Tax</span><b class="index">${{Cart::instance('cart')->tax()}}</b></p>
+                            <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
+                            <p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::instance('cart')->total()}}</b></p>
+                        @endif
                     </div>
                     <div class="checkout-info">
+                        @if(!Session::has('coupon'))
                         <label class="checkbox-field">
-                            <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have promo code</span>
+                            <input class="frm-input " name="have-code" id="have-code" value="1" type="checkbox" wire:model="haveCouponCode"><span>I have coupon code</span>
                         </label>
+                        @if($haveCouponCode == 1)
+                            <!-- New div with form in Eps 37 -->
+                                <div class="summary-item">
+                                    <form wire:submit.prevent="applyCouponCode">
+                                        <h4 class="title-box">Coupon Code</h4> 
+                                        @if(Session::has('coupon_message'))
+                                            <div class="alert alert-danger" role="danger">{{Session::get('coupon_message')}}</div>
+                                        @endif
+                                        <p class="row-in-form">
+                                            <label for="coupon-code">Enter your coupon code:</label>
+                                            <input type="text" name="coupon-code" wire:model="couponCode"/>
+                                        </p>
+                                        <button type="submit" class="btn btn-small">Apply</button>
+                                    </form>
+                                </div>
+                            @endif
+                        @endif
                         <a class="btn btn-checkout" href="checkout.html">Check out</a>
                         <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
                     </div>
@@ -76,7 +102,7 @@
                 </div>
 
                 <div class="wrap-iten-in-cart">
-                    <h3 class="title-box" style="border-bottom: 1px solid; padding-bottom: 15px;">{{Cart::instance('saveForLater')->count()}} Item(s) saved for later</h3>
+                    <h3 class="title-box" style="border-bottom: 1px solid; padding-bottom: 15px;">{{Cart::instance('saveForLater')->count()}} item(s) Saved For Later</h3>
                     @if(Session::has('s_success_message'))
                         <div class="alert alert-success">
                             <strong>Success</strong>{{ Session::get('s_success_message')}}
@@ -94,12 +120,14 @@
                                         <a class="link-to-product" href="{{ route('product.details', ['slug'=>$item->model->slug]) }}">{{ $item->model->name }}</a>
                                     </div>
                                     <div class="price-field produtc-price"><p class="price">${{ $item->model->regular_price }}</p></div>
+                                   
                                     <div class="quantity">
-                                        <p class="text-center"><a href="#" wire:click.prevent="moveToCart('{{$item->rowId}}')">Move back to Cart</a></p>
+                                        <p class="text-center"><a href="#" wire:click.prevent="moveToCart('{{$item->rowId}}')">Move to Cart</a></p>
                                     </div>
+
                                     <div class="delete">
                                         <a href="#" class="btn btn-delete" title="" wire:click.prevent="deleteFromSaveForLater('{{$item->rowId}}')">
-                                            <span>Delete from save for later</span>
+                                            <span>Delete from saved for later</span>
                                             <i class="fa fa-times-circle" aria-hidden="true"></i>
                                         </a>
                                     </div>
@@ -107,7 +135,7 @@
                             @endforeach												
                         </ul>
                     @else
-                        <p>No item Save for later</p>
+                        <p>No item saved for later</p>
                     @endif
                 </div>
     
