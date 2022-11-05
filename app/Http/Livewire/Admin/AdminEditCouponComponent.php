f71d4f28320115432894d9a3220c66f481d0434b
Ahmad <amgo85@gmail.com>
2022-11-05 17:15:35 +0400

    video 38 completed

--- app/Http/Livewire/Admin/AdminEditCouponComponent.php
+++ app/Http/Livewire/Admin/AdminEditCouponComponent.php
@@ -12,6 +12,7 @@ class AdminEditCouponComponent extends Component
     public $value;
     public $cart_value;
     public $coupon_id;
+    PUBLIC $expiry_date;
 
     public function mount($coupon_id)
     {
@@ -21,6 +22,7 @@ class AdminEditCouponComponent extends Component
         $this->value = $coupon->value;
         $this->cart_value = $coupon->cart_value;
         $this->coupon_id = $coupon->id;
+        $this->expiry_date = $coupon->expiry_date;
     }
 
     public function updated($fields)
@@ -29,7 +31,8 @@ class AdminEditCouponComponent extends Component
             'code' => 'required',
             'type' => 'required',
             'value' => 'required|numeric',
-            'cart_value' => 'required|numeric'
+            'cart_value' => 'required|numeric',
+            'expiry_date' => 'required'
         ]);
     }
 
@@ -40,12 +43,14 @@ class AdminEditCouponComponent extends Component
             'type' => 'required',
             'value' => 'required|numeric',
             'cart_value' => 'required|numeric',
+            'expiry_date' => 'required',
         ]);
         $coupon = Coupon::find($this->coupon_id);
         $coupon->code = $this->code;
         $coupon->type = $this->type;
         $coupon->value = $this->value;
         $coupon->cart_value = $this->cart_value;
+        $coupon->expiry_date = $this->expiry_date;
         $coupon->save();
         session()->flash('message','Coupon has been updated successfully!');
     }
